d6b445361f68576f8442db915dc65c57f1870e8c
yuvrajwable-7 <yuvrajwable305@gmail.com>
2026-01-13 09:52:57 +0530

    Add FAQ using JS DOM

--- script.js
+++ script.js
@@ -1,26 +1,8 @@
-let input = document.getElementById("input");
-let ulist = document.getElementById("ulist");
-let button = document.getElementById("butt");
+const faq = document.getElementById("faq");
 
-button.addEventListener("click", taskOn);
-
-function taskOn() {
-    const contain = input.value;
-
-    let list = document.createElement("li");
-    list.innerText = contain;
-
-    ulist.appendChild(list);
-
-    list.addEventListener("click", () => {
-        list.classList.toggle("done");
-
-    });
-
-    list.addEventListener("dblclick", () => {
-        ulist.removeChild(list);
-    })
-
-
-
-}
+faq.addEventListener("click", (e) => {
+    if (e.target.classList.contains("question")) {
+        const answer = e.target.nextElementSibling;
+        answer.style.display = answer.style.display === "block" ? "none" : "block";
+    }
+});
